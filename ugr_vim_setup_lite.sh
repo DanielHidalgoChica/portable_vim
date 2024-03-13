6239b6125e823285ed629923684a6efa79230f73
Daniel Hidalgo Chcia <daniel5000.dhc@gmail.com>
2024-03-13 10:44:31 +0100

    Proxy settings and parameter control added to script

--- ugr_vim_setup_lite.sh
+++ ugr_vim_setup_lite.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]; then
+    echo "Use: $0 <folder_containing_script>"
+    exit 1
+fi
 usb=$1
 
 cp -r "$usb"/.vim "$usb"/.vimrc "$usb"/vim.tgz "$HOME" # Copiamos todo a home
@@ -14,5 +18,14 @@ source ~/.bash_profile
 # Switch ctrl y caps
 setxkbmap -option ctrl:swapcaps
 
+proxy=$http_proxy
+sproxy=$https_proxy
 
+if [[ -z "$proxy" || -z "$sproxy" ]]; then
+    echo "http_proxy env. variable is not set"
+        exit 1
+fi
+
+git config --global http.proxy "$proxy"
+git config --global https.proxy "$sproxy"
 
